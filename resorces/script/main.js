df80af6e9d31c6b5dccaabed509b7d3e7654788a
choihahye <coolage512@gmail.com>
2021-05-02 16:21:42 +0900

    complete mainpage

--- resorces/script/main.js
+++ resorces/script/main.js
@@ -1,3 +1,4 @@
+//전체페이지
 let openButton = document.querySelector('.open-button');
 let allmenuWrap = document.querySelector('.allmenu-wrap');
 openButton.addEventListener('click', function () {
@@ -6,4 +7,22 @@ openButton.addEventListener('click', function () {
 let closeButton = document.querySelector('.close-button');
 closeButton.addEventListener('click', function () {
     allmenuWrap.classList.remove('visible');
+});
+
+
+//비주얼페이지
+$('.visual-slide').slick({
+    // autoplay: true,
+    // autoplaySpeed: 2000,
+    pauseOnHover: true,
+    dots: true,
+});
+
+//홀트패밀리
+$('.partner-slider').slick({
+    slidesToShow: 6,
+    slidesToScroll: 6,
+    // vertical: true,
+    // verticalSwiping: true,
+    speed: 1000
 });
