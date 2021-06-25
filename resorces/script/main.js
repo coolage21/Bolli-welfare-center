7a6172b69096b87586164a699456d8679d92940f
choihahye <coolage512@gmail.com>
2021-06-25 18:16:55 +0900

    Complete

--- resorces/script/main.js
+++ resorces/script/main.js
@@ -1,16 +1,11 @@
-//비주얼페이지
+//main-visual section
 $('.visual-slide').slick({
-    // autoplay: true,
-    // autoplaySpeed: 2000,
     pauseOnHover: true,
     dots: true,
 });
 
-//홀트패밀리
+//main-partner section
 $('.partner-slider').slick({
     slidesToShow: 6,
     slidesToScroll: 6,
-    // vertical: true,
-    // verticalSwiping: true,
-    speed: 1000
 });
