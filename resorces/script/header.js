456fd432ae7a397c6899e2e6add5344a2aca169f
choihahye <coolage512@gmail.com>
2021-06-29 16:59:42 +0900

    Modifying02

--- resorces/script/header.js
+++ resorces/script/header.js
@@ -1,10 +1,15 @@
-//header-allmenu
-let openButton = document.querySelector('.open-button');
-let allmenuWrap = document.querySelector('.allmenu-wrap');
-openButton.addEventListener('click', function (){
-    allmenuWrap.classList.add('visible');
+//header,footer
+$('.header-include').load('header.html', function(){
+    let openButton = document.querySelector('.open-button');
+    let allmenuWrap = document.querySelector('.allmenu-wrap');
+    openButton.addEventListener('click', function (){
+        allmenuWrap.classList.add('visible');
+    })
+    let closeButton = document.querySelector('.close-button');
+    closeButton.addEventListener('click', function (){
+        allmenuWrap.classList.remove('visible');
+    });
 })
-let closeButton = document.querySelector('.close-button');
-closeButton.addEventListener('click', function (){
-    allmenuWrap.classList.remove('visible');
-});
+
+$('.footer-include').load('footer.html')
+
